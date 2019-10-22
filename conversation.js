5c223d84c7c11ce227a02c858acdcc2fa80e39a7
xietf <826597263@qq.com>
2019-10-22 23:39:41 +0800

    feat: 顶层选项

--- conversation.js
+++ conversation.js
@@ -8,4 +8,39 @@
  * - 错误提示
  */
 
+const inquirer = require("inquirer");
 
+const topOption = {
+  type: "list",
+  message: "选择需要进行的操作",
+  choices: [
+    {
+      name: "下载\\更新接口",
+      key: "down",
+    },
+    {
+      name: "查询接口",
+      key: "search",
+    },
+    {
+      name: "接口管理",
+      key: "manage",
+    },
+    {
+      name: "增加接口",
+      key: "add",
+    },
+    {
+      name: "修改接口",
+      key: "edit",
+    },
+    {
+      name: "删除接口",
+      key: "delet",
+    },
+  ],
+};
+
+inquirer.prompt(topOption).then(answers => {
+  console.log(answers);
+});
